b9b2b670966b3b3c42fdc62d1053c2d88e87f131
Farisa Damayanti <r11solmayo0w@gmail.com>
2026-09-07 16:06:23 +0700

    seeeder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\ProdukController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::resource('produk', ProdukController::class);
+Route::resource('penjualan', PenjualanController::class);
