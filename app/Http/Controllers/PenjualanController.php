8a4e4e4e846ad0e429bf59e9ce124648b5581716
Farisa Damayanti <r11solmayo0w@gmail.com>
2026-09-10 08:47:51 +0700

    controller blm fix

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -2,37 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\helper;
+use App\Models\Penjualan;
 use Illuminate\Http\Request;
 
 class PenjualanController extends Controller
 {
-    public function index ()
+    public function index(Request $request)
     {
+        $query = Penjualan::with('detailPenjualans');
 
+        $query = helper::tanggal(
+            $query,
+            $request->start_date,
+            $request->end_date
+        );
+
+        $penjualans = $query->get();
+
+        return view(
+            'penjualan.index',
+            compact('penjualans')
+        );
     }
 
-    public function create () 
+    public function create()
     {
-
+        return view('penjualan.create');
     }
 
-    public function store () 
+    public function store(Request $request)
     {
+        $request->validate([
+            'tanggal' => 'required|date',
+            'total_pemasukan' => 'required|numeric',
+        ]);
 
+        Penjualan::create([
+            'tanggal' => $request->tanggal,
+            'total_pemasukan' => $request->total_pemasukan,
+        ]);
+
+        return redirect()->route('penjualan.index');
     }
 
-    public function edit () 
+    public function edit($id)
     {
+        $penjualan = Penjualan::findOrFail($id);
 
+        return view(
+            'penjualan.edit',
+            compact('penjualan')
+        );
     }
 
-    public function update ()
+    public function update(Request $request, $id)
     {
+        $request->validate([
+            'tanggal' => 'required|date',
+            'total_pemasukan' => 'required|numeric',
+        ]);
 
+        $penjualan = Penjualan::findOrFail($id);
+
+        $penjualan->update([
+            'tanggal' => $request->tanggal,
+            'total_pemasukan_pemasukan' => $request->total_pemasukan,
+        ]);
+
+        return redirect()->route('penjualan.index');
     }
 
-    public function destroy ()
+    public function destroy($id)
     {
+        $penjualan = Penjualan::findOrFail($id);
 
+        $penjualan->delete();
+
+        return redirect()->route('penjualan.index');
     }
 }
